919dff9352ab8c43c452420d00ca8a674f49ea16
Vitalie Bancu <vitalie.bancu@statistica.gov.md>
2021-07-08 12:25:42 +0300

    update

---
+++ java/YourScript.js
@@ -0,0 +1,24 @@
+function Link1() {
+  window.location.assign("index.html")
+}
+
+
+function Link2() {
+  window.location.assign("index1.html")
+}
+
+
+
+function newDoc1() {
+  window.location.assign("https://www.w3schools.com")
+}
+
+
+function Back_to_main() {
+  window.location.assign("../index.php")
+}
+
+
+
+
+
